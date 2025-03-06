9c0e5a87c8448fd459d512d93fc51d7aabf7797f
mesutyilaiz166@gmail.com <mesutyilaiz16@gmail.com>
2025-03-06 17:59:37 +0300

    GoogleCloud Vision Api ile resim üzerindeki yazıları okuma

---
+++ NetCoreAI.Project08_GoogleCloudVision/Program.cs
@@ -0,0 +1,37 @@
+﻿using Google.Cloud.Vision.V1;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        Console.Write("Resim yolunu giriniz: ");
+        string imagePath = Console.ReadLine();
+        Console.WriteLine();
+
+        string credentialPath = "buraya googleden gelen json dosyası gelecek";
+        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath);
+
+        try
+        {
+            var client = ImageAnnotatorClient.Create();
+
+            var image = Image.FromFile(imagePath);
+            var response = client.DetectText(image);
+            Console.WriteLine("Resimdeki metin: ");
+            Console.WriteLine();
+
+            foreach (var annotation in response)
+            {
+                if (!string.IsNullOrEmpty(annotation.Description))
+                {
+                    Console.WriteLine(annotation.Description);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+
+            Console.WriteLine($"Bir hata oluştu: {ex.Message} ");
+        }
+    }
+}
