c0203667dd548d7e44a5ea6a20966331b96a1036
mesutyilaiz166@gmail.com <mesutyilaiz16@gmail.com>
2025-03-06 17:06:21 +0300

    Tesserect OCR ile resim üzerindeki metinleri okuma

---
+++ NetCoreAI.Project07_TesseractOcr/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tesseract;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Karakter okuması yapılacak resim yolu: ");
+        string imagepath = Console.ReadLine();
+        Console.WriteLine();
+
+        string testDataPath = @"C:\tessdata";
+
+        try
+        {
+            using (var engine = new TesseractEngine(testDataPath, "tur", EngineMode.Default))
+            {
+                using (var img = Pix.LoadFromFile(imagepath))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        string text = page.GetText();
+                        Console.WriteLine("Resimden okunan metin: ");
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+
+            Console.WriteLine($"Bir hata oluştu: {ex.Message}");
+        }
+    }
+}
