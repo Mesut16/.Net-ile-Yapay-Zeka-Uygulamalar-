376e355aebe3d2f2bf14917020f9c72ee0fbe2b3
mesutyilaiz166@gmail.com <mesutyilaiz16@gmail.com>
2025-03-06 19:03:43 +0300

    Text To Speech

---
+++ NetCoreAI.Project10_TextToSpeech/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Speech.Synthesis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+
+        speechSynthesizer.Volume = 100;
+        speechSynthesizer.Rate = -1;
+
+        Console.Write("Metni girin: ");
+        string input = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(input))
+        {
+            speechSynthesizer.Speak(input);
+        }
+    }
+}
